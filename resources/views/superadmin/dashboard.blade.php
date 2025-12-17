bf6aa59d0aaf9cb95a584a0ff9c5b89e67c5334d
Sandy <jonu89@gmail.com>
2025-12-17 14:29:40 +0530

    work file

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -4,7 +4,7 @@
 @section('page-title', 'SuperAdmin Dashboard')
 
 @section('content')
-<div class="space-y-6">
+<div class="space-y-6"> 
     <!-- Statistics Cards -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <!-- Total Vendors Card -->
