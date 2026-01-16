394ba5f661f1b57ad4a98a014c103e7f45ecb671
Sandy <jonu89@gmail.com>
2026-01-16 15:10:29 +0530

    QwaitingAi Changes Done

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,8 +67,7 @@ class User extends Authenticatable
         'must_change_password',
         'login_datetime',
 		'is_superadmin',
-        'sms_credits_balance',
-        'additional_info',
+        'sms_credits_balance'
     ];
 
 
@@ -138,8 +137,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
         'is_admin' => 'boolean',
-        'assigned_permissions' => 'array',
-        'additional_info' => 'array',
+        'assigned_permissions' => 'array'
     ];
 
     public function setLocationsAttribute($value)
