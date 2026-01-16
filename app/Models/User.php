03543e611e1bf64a169bce679852a31d4c3e6103
stellenmohan <mohan@stelleninfotech.com.au>
2026-01-16 14:58:03 +0530

    revert user model change

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,7 +68,6 @@ class User extends Authenticatable
         'login_datetime',
 		'is_superadmin',
         'sms_credits_balance',
-        'additional_info',
     ];
 
 
@@ -138,8 +137,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
         'is_admin' => 'boolean',
-        'assigned_permissions' => 'array',
-        'additional_info' => 'array',
+        'assigned_permissions' => 'array'
+        
     ];
 
     public function setLocationsAttribute($value)
